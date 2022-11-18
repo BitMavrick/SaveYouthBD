dc65bed14d4bb96d4632c9f92ce9023043d476c6
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-11-18 14:33:27 +0600

    update forms

--- resources/views/components/partials/modals.blade.php
+++ resources/views/components/partials/modals.blade.php
@@ -21,12 +21,14 @@
                         <label for="exampleInputPassword1">Password</label>
                         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                     </div>
+
                     <div class="form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">Remeber Me</label>
                         <br>
                         <br>
                     </div>
+
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
@@ -36,13 +38,12 @@
 </div>
 
 <!-- Register -->
-
 <div class="modal fade" id="modal-register" tabindex="-1" role="dialog" aria-labelledby="modal-register"
     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">welcome to SaveYouthBD.</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Welcome to SaveYouthBD.</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -50,15 +51,33 @@
             <div class="modal-body">
                 <form>
                     <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" aria-describedby="emailHelp"
+                            placeholder="Enter your name">
+
+                    </div>
+                    <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                             placeholder="Enter email">
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                             else.</small>
                     </div>
+
+                    <select class="form-control form-group">
+                        <option selected>I'm a patient</option>
+                        <option>I'm a Doctor</option>
+                        <option>I'm a Staff</option>
+                    </select>
+
+
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
                         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword2">Re-enter Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
                     </div>
                     <div class="form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
