29789eea65f918f713ebfc093d1fd1cd3fb6cd38
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-11-18 12:22:38 +0600

    Done making component

---
+++ resources/views/components/master.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <!-- ** Basic Page Needs ** -->
+    <meta charset="utf-8" />
+    <title>Megakit | HTML5 Agency Template</title>
+
+    <!-- ** Mobile Specific Metas ** -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="Agency HTML Template" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
+    <meta name="author" content="Themefisher" />
+    <meta name="generator" content="Themefisher Html5 Agency Template v1.0" />
+
+    <!-- theme meta -->
+    <meta name="theme-name" content="megakit" />
+
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css" />
+    <!-- Icon Font Css -->
+    <link rel="stylesheet" href="plugins/themify/css/themify-icons.css" />
+    <link rel="stylesheet" href="plugins/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="plugins/magnific-popup/magnific-popup.css" />
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="plugins/slick/slick.css" />
+    <link rel="stylesheet" href="plugins/slick/slick-theme.css" />
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!--Favicon-->
+    <link rel="icon" href="images/favicon.png" type="image/x-icon" />
+</head>
+
+<body>
+    {{ $slot }}
+
+    <!-- 
+Essential Scripts
+=====================================-->
+    <!-- Main jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4.3.1 -->
+    <script src="plugins/bootstrap/bootstrap.min.js"></script>
+    <!--  Magnific Popup-->
+    <script src="plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <!-- Slick Slider -->
+    <script src="plugins/slick/slick.min.js"></script>
+    <!-- Counterup -->
+    <script src="plugins/counterup/jquery.waypoints.min.js"></script>
+    <script src="plugins/counterup/jquery.counterup.min.js"></script>
+
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
+    <script src="plugins/google-map/map.js" defer></script>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ resources/views/components/partials/scroll-up.blade.php
@@ -0,0 +1,4 @@
+<!--Scroll to top-->
+<div id="scroll-to-top" class="scroll-to-top">
+    <span class="icon fa fa-angle-up"></span>
+</div>
