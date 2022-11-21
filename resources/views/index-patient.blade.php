39077cd6c25f98ad505ad250ad7ef11fa4f55a2c
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-11-22 01:57:20 +0600

    enough for tonight

--- resources/views/index-patient.blade.php
+++ resources/views/index-patient.blade.php
@@ -3,7 +3,7 @@
 
     <div class="container">
 
-        <h3 class="mt-4">Available Doctor</h3>
+        <h3 class="mt-4">Available Doctors</h3>
         <hr>
 
         <div class="row">
@@ -20,11 +20,22 @@
 
         </div>
 
+        <h3 class="mt-4">Popular Doctors</h3>
+        <hr>
 
+        <div class="row">
 
+            <div class="col-3 my-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-
 
     <x-partials.footer />
 </x-master>
