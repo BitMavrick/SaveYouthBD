dce5c5ec417055300ab4f3ec5dcc44f252cce2d1
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-11-22 00:25:32 +0600

    Done making staff profile

--- resources/views/components/partials/navbar-staff.blade.php
+++ resources/views/components/partials/navbar-staff.blade.php
@@ -1,27 +1,4 @@
 <header class="navigation">
-    <div class="header-top">
-        <div class="container">
-            <div class="row justify-content-between align-items-center">
-                <div class="col-lg-2 col-md-4">
-                    <div class="header-top-socials text-center text-lg-left text-md-left">
-                        <a href="https://www.facebook.com/themefisher" aria-label="facebook"><i
-                                class="fab fa-facebook-f"></i></a>
-                        <a href="https://twitter.com/themefisher" aria-label="twitter"><i
-                                class="fab fa-twitter"></i></a>
-                        <a href="https://github.com/themefisher/" aria-label="github"><i class="fab fa-github"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-10 col-md-8 text-center text-lg-right text-md-right">
-                    <div class="header-top-info mb-2 mb-md-0">
-                        <a href="tel:+23-345-67890">Call Us : <span>+23-345-67890</span></a>
-                        <a href="mailto:support@gmail.com"><i
-                                class="fas fa-envelope mr-2"></i><span>support@gmail.com</span></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div id="navbar">
         <div class="container">
             <div class="row">
--- resources/views/components/partials/update-modal.blade.php
+++ resources/views/components/partials/update-modal.blade.php
@@ -98,7 +98,7 @@
 </div>
 
 
-<!-- For Doctor -->
+<!-- For Staff -->
 <div class="modal fade" id="modal-update-staff" tabindex="-1" role="dialog" aria-labelledby="modal-register"
     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
@@ -129,66 +129,6 @@
                         <input type="text" name="phone" class="form-control" id="name" aria-describedby="emailHelp"
                             placeholder="Enter your phone number" value="{{ $profile_data->phone }}">
                     </div>
-
-                    <div class="form-group">
-                        <label for="gender">Gender</label>
-                        <select name="gender" class="form-control form-group">
-                            <option selected disabled>Select gender</option>
-                            <option {{ $profile_data->gender === "male" ? "selected" : "" }} value="male">Male</option>
-                            <option {{ $profile_data->gender === "female" ? "selected" : "" }} value="female">Female
-                            </option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="address">Address</label>
-                        <input type="text" name="address" class="form-control" id="address" aria-describedby="emailHelp"
-                            placeholder="Enter your living address" value="{{ $profile_data->address }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="date_of_birth">Date of Birth</label>
-                        <input type="date" name="date_of_birth" class="form-control" id="date_of_birth"
-                            aria-describedby="emailHelp" placeholder="Enter your living address"
-                            value="{{ $profile_data->date_of_birth }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="educational_qualification">Educational Qualification</label>
-                        <input type="text" name="educational_qualification" class="form-control"
-                            id="educational_qualification" aria-describedby="emailHelp"
-                            placeholder="Enter your educational qualification"
-                            value="{{ $profile_data->educational_qualification }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="experties">Experties</label>
-                        <input type="text" name="experties" class="form-control" id="experties"
-                            aria-describedby="emailHelp" placeholder="Enter your experties"
-                            value="{{ $profile_data->experties }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="working_organization">Organization</label>
-                        <input type="text" name="working_organization" class="form-control" id="working_organization"
-                            aria-describedby="emailHelp" placeholder="Enter your current working organization"
-                            value="{{ $profile_data->working_organization }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="website">Website</label>
-                        <input type="text" name="website" class="form-control" id="website" aria-describedby="emailHelp"
-                            placeholder="Enter your personal website address" value="{{ $profile_data->website }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="description">About You</label>
-                        <input type="text" name="description" class="form-control" id="description"
-                            aria-describedby="emailHelp" placeholder="Write something about yourself"
-                            value="{{ $profile_data->description }}">
-                    </div>
-
-
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
