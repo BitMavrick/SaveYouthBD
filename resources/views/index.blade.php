1f25da4c3504f0c163717ef2f2d22eebb5a66968
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-11-24 03:11:31 +0600

    breakpoint

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,12 +9,13 @@
             <div class="row">
                 <div class="col-lg-9 col-md-10">
                     <div class="block">
-                        <span class="d-block mb-3 text-white text-capitalize">Prepare for new future</span>
+                        <span class="d-block mb-3 text-white text-capitalize">Save The New Generation</span>
                         <h1 class="animated fadeInUp mb-5">
-                            Our work is <br />presentation of our <br />capabilities.
+                            Our work is <br />is to protect Youth from<br />Drugs.
                         </h1>
-                        <a href="#" class="btn btn-main animated fadeInUp btn-round-full" aria-label="Get started">Get
-                            started<i class="btn-icon fa fa-angle-right ml-2"></i></a>
+                        <a href="#" class="btn btn-main animated fadeInUp btn-round-full" aria-label="Get started"
+                            data-toggle="modal" data-target="#modal-register">Register Now<i
+                                class="btn-icon fa fa-angle-right ml-2"></i></a>
                     </div>
                 </div>
             </div>
