b49aaaf56950b5fe552d4d23107812d00b1fd607
Mehedi Hasan <email.mehedi.bd@gmail.com>
2022-11-24 01:13:11 +0600

    preparing for taking request

---
+++ app/Models/Request.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Request extends Model
+{
+    use HasFactory;
+
+    protected $table = 'requests';
+
+    protected $fillable = [
+        'name',
+        'patient_id',
+        'rehab_id',
+        'phone',
+        'email',
+        'address',
+    ];
+}
---
+++ database/migrations/2022_11_23_190714_create_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('patient_id')->nullable();
+            $table->string('rehab_id')->nullable();
+            $table->string('phone');
+            $table->string('email');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+};
